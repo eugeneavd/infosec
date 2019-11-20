a7c3b62f06480750e5bf4b523b1506b11a63efcc
Evgeny <eugeneavd@github.com>
2019-11-20 16:55:10 +0300

    Some changes in IntModuloP class

--- IntModuloP.cpp
+++ IntModuloP.cpp
@@ -1,1 +1,13 @@
 #include "IntModuloP.h"
+#include "includes.h"
+
+IntModuloP operator + (const IntModuloP &a, const IntModuloP &b){
+    IntModuloP res;
+    res.z = (a.z + b.z) % a.p;
+}
+
+IntModuloP operator - (const IntModuloP &a, const IntModuloP &b){
+    IntModuloP res;
+    res.z = (a.z - b.z) % a.p;
+}
+
--- IntModuloP.h
+++ IntModuloP.h
@@ -1,7 +1,33 @@
 #ifndef INFOSEC_INTMODULOP_H
 #define INFOSEC_INTMODULOP_H
 
-
 typedef int IntModuloP;
+
+class IntModuloP
+{
+private:
+    int p;
+    int z;
+public:
+    IntModuloP() = default;
+    IntModuloP(int a);
+    ~IntModuloP();
+    friend IntModuloP operator + (const IntModuloP &a, const IntModuloP &b);
+    friend IntModuloP operator - (const IntModuloP &a, const IntModuloP &b);
+    friend IntModuloP operator * (const IntModuloP &a, const IntModuloP &b);
+    friend IntModuloP operator / (const IntModuloP &a, const IntModuloP &b);
+    friend IntModuloP Inverse (const IntModuloP &a);
+};
+
+IntModuloP::IntModuloP(int a)
+{
+}
+
+IntModuloP::~IntModuloP()
+{
+}
+
+
+
 
 #endif //INFOSEC_INTMODULOP_H
